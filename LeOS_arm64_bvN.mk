50e45fabc2c304033aec2a5561124e0837cde55c
harvey186 <harvey186@hotmail.com>
2022-12-03 11:36:14 +0100

    Commits Dec, 1st

--- LeOS_arm64_bvN.mk
+++ LeOS_arm64_bvN.mk
@@ -2,7 +2,7 @@ $(call inherit-product, device/LeOS/base-pre.mk)
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/LeOS/base-secure.mk)
 $(call inherit-product, vendor/LeOS/leos.mk)
-$(call inherit-product, device/LeOS/lineage.mk)
+#$(call inherit-product, device/LeOS/lineage.mk)
 
 
 PRODUCT_NAME := lineage_arm64_bvN
